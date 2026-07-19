9a080e05120430ce8c950271187c97ff62794561
kanishgab-tech <kanishga.b@gmail.com>
2026-07-19 18:33:30 -0500

    FirstCommit

--- app/layout.js
+++ app/layout.js
@@ -3,15 +3,20 @@ import "./globals.css";
 import { AppContextProvider } from "@/context/AppContext";
 import { Toaster } from "react-hot-toast";
 
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+
 const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
 
 export const metadata = {
-  title: "QuickCart - GreatStack",
-  description: "E-Commerce with Next.js ",
+  title: "KanSan Crackers",
+  description: "Shop with KanSan Crackers!!",
 };
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
       <html lang="en">
         <body className={`${outfit.className} antialiased text-gray-700`} >
           <Toaster />
@@ -20,5 +25,6 @@ export default function RootLayout({ children }) {
           </AppContextProvider>
         </body>
       </html>
+      </ClerkProvider>
   );
 }
---
+++ config/inngest.js
@@ -0,0 +1,64 @@
+// src/inngest/client.ts
+import { Inngest } from "inngest";
+import { connect } from "mongoose";
+import connectDB from "./db";
+import User from "@/models/User";
+
+export const inngest = new Inngest({ id: "kansan-next" });
+
+//Inngest function to save user data to a database
+
+export const syncUserCreation = inngest.createFunction(
+    {
+        id:'sync-user-from-clerk'
+    },
+    {
+        event: 'clerk/user.created'
+    },
+    async ({event}) => {
+        const { id, first_name, last_name, email_addresses, image_url} = event.data
+        const userData = {
+            _id:id,
+            email:email_addresses[0].email_addresses,
+            name: first_name + ' ' + last_name,
+            image_Url: image_url
+        }
+        await connectDB()
+        await User.create(userData)
+    }
+)
+
+export const syncUserUpdate = inngest.createFunction(
+    {
+        id:'update-user-from-clerk'
+    },
+    {
+        event: 'clerk/user.updated'
+    },
+    async ({event}) => {
+        const { id, first_name, last_name, email_addresses, image_url} = event.data
+        const userData = {
+            _id:id,
+            email:email_addresses[0].email_addresses,
+            name: first_name + ' ' + last_name,
+            image_Url: image_url
+        }
+        await connectDB()
+        await User.findByIdAndUpdate(id,userData)
+    }
+)
+
+export const syncUserDeletion = inngest.createFunction(
+    {
+        id:'delete-user-from-clerk'
+    },
+    {
+        event: 'clerk/user.deleted'
+    },
+    async ({event}) => {
+        const { id } = event.data
+                
+        await connectDB()
+        await User.findByIdAndDelete(id)
+    }
+)
