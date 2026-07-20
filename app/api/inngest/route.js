d53e0c61714b5f30729cb8b3dae5bed2c372a460
kanishgab-tech <kanishga.b@gmail.com>
2026-07-19 21:48:32 -0500

    commit5

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -2,6 +2,9 @@ import { serve } from "inngest/next";
 import { inngest } from "@/config/inngest";
 import {syncUserCreation, syncUserDeletion, syncUserUpdate } from "@/config/inngest";
 
+// Force Next.js to treat this route as completely dynamic
+export const dynamic = "force-dynamic";
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
