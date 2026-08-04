5e63423b50e8d0afbd78fdc491ae3556324ffc5a
kanishgab-tech <kanishga.b@gmail.com>
2026-08-04 07:33:43 -0500

    email fix

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -56,7 +56,7 @@ export const createUserOrder = inngest.createFunction(
             if (targetEmail) {
                 try {
                     await resend.emails.send({
-                        from: "Shop Orders <kanishga.b@gmail.com>", // Replace with your verified Resend domain
+                        from: "Shop Orders <kanishga.b@gmail.com>", // Replace with your verified Resend domain, update
                         to: targetEmail.trim(),
                         subject: `Order Confirmation - ${payload.orderNumber}`,
                         html: generateInvoiceHTML(payload, orderPayload.date) // Call HTML template builder
